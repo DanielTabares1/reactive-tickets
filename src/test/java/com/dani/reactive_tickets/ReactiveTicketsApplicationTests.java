bdbe9bde54ca54c8d8fc07f7ce3b09e195ea9fc8
Daniel Tabares <daniel.tabares1@udea.edu.co>
2026-03-04 12:33:47 -0500

    HU-01: Created base spring project

---
+++ src/test/java/com/dani/reactive_tickets/ReactiveTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dani.reactive_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
